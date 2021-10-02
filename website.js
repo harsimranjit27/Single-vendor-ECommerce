ec81a6335fecdb6c41046f30624356d3405505b2
harsimranjit27 <harsimran2731@gmail.com>
2021-10-02 16:12:08 +0530

    Added functionality to login,go to cart button when no user logged in; added functionality to add items to cart, increase-decrease value of items; alerting when user not loggedIn and trying to use functionality that requires to login; added functionality to checkout button which removes items from cart of individual user

--- website.js
+++ website.js
@@ -1,7 +1,3 @@
-/**
- * get products from local storage
- * add them to website from localStorage
- */
 let productsToDisplayDiv = document.querySelector(".products");
 let loginBtn = document.querySelector("#userName");
 let goToCartBtn = document.getElementById("cartBtn");
@@ -20,12 +16,28 @@ function getLocalStorage() {
 let productsArr = getLocalStorage();
 console.log(productsArr);
 
+    // if no user has logged in
+    loginBtn.addEventListener("click", (event) => {
+        if (localStorage.getItem("loggedInEMail") === null || localStorage.getItem("loggedInEMail") === "" || localStorage.getItem("loggedInEMail") === undefined) {
+            document.querySelector(".login_container").style.display = "inherit";
+            document.querySelector("#website_container").style.display = "none";
+            document.querySelector(".signUpContainer").style.display = "none";
+            document.querySelector(".admin_container").style.display = "none";
+            document.querySelector(".cart_container").style.display = "none";
+        }
+    });
+    goToCartBtn.addEventListener("click",(event)=>{
+        if (localStorage.getItem("loggedInEMail") === null || localStorage.getItem("loggedInEMail") === undefined || localStorage.getItem("loggedInEMail") === "") {
+            alert("Please login first to go to cart");
+        }
+    });
+
 if (productsArr.length === 0) {
     document.getElementById("noProductsAvailable").style.display = "inherit";
     document.getElementById("productsAvailable").style.display = "none";
-} else {
+}
+else {
     // Add products to website
-    // console.log(productsArr);
     productsArr.forEach(products => {
         let element = {
             productImage: products.productImage,
@@ -82,155 +94,170 @@ if (productsArr.length === 0) {
 
         productsToDisplayDiv.appendChild(productDiv);
     });
+
     productsAddedToWebsite = true;
     if (productsAddedToWebsite) {
         let addToCartBtn = document.querySelectorAll(".addToCartBtn");
-        // console.log(addToCartBtn);
         let userArr = JSON.parse(localStorage.getItem("user_details"));
         let loggedInEMail = localStorage.getItem("loggedInEMail");
 
         addToCartBtn.forEach(element => {
             element.addEventListener("click", () => {
-                element.innerText = "Added !!";
-                userArr.forEach(user => {
-                    if (user.emailAddress === loggedInEMail.replace(/['"]+/g, '')) {
+                // user is not logged in
+                if(localStorage.getItem("loggedInEMail") === null || localStorage.getItem("loggedInEMail") === undefined || localStorage.getItem("loggedInEMail") === ""){
+                    alert("Please login to add items to cart");
+                }
 
-                        let products = JSON.parse(localStorage.getItem("product_info"));
-                        let name = element.parentNode.childNodes[1].innerText;
-                        let price = element.parentNode.childNodes[3].innerText;
-                        let img = (products.find(e => e.productName === name)).productImage;
-                        let details = {
-                            name,
-                            price,
-                            img
+                // user is logged in
+                else if (localStorage.getItem("loggedInEMail").replace(/['"]+/g, '') !== null || localStorage.getItem("loggedInEMail").replace(/['"]+/g, '') !== undefined || localStorage.getItem("loggedInEMail").replace(/['"]+/g, '') !== "") { 
+                    element.innerText = "Added !!";
+                    userArr.forEach(user => {
+                        if (user.emailAddress === loggedInEMail.replace(/['"]+/g, '')) {
+                            let products = JSON.parse(localStorage.getItem("product_info"));
+                            let name = element.parentNode.childNodes[1].innerText;
+                            let price = element.parentNode.childNodes[3].innerText;
+                            let img = (products.find(e => e.productName === name)).productImage;
+                            let details = {
+                                name,
+                                price,
+                                img
+                            }
+                            console.log(details);
+                            user.cart.push(details);
+                            localStorage.setItem("user_details", JSON.stringify(userArr));
                         }
-                        console.log(details);
-                        user.cart.push(details);
-                        // console.log(user.cart);
-                        localStorage.setItem("user_details", JSON.stringify(userArr));
-                    }
-                });
+                    });
+                }
+                
             });
-
         });
     }
-}
 
-goToCartBtn.addEventListener("click", checkLoggedIn);
-
-function checkLoggedIn() {
-    let loggedInEMail = localStorage.getItem("loggedInEMail");
-
-    let users = JSON.parse(localStorage.getItem("user_details"));
-    let currUser = users.find(e => e.emailAddress === loggedInEMail.replace(/['"]+/g, ''));
-
-    // not logged in
-    if (loggedInEMail === "" || loggedInEMail === undefined || loggedInEMail === null) {
-        document.querySelector("#website_container").style.display = "none";
-        document.querySelector(".signUpContainer").style.display = "none";
-        document.querySelector(".admin_container").style.display = "none";
-        document.querySelector(".login_container").style.display = "inherit";
+    let returnBtn = document.querySelector("#backToHome");
+    returnBtn.addEventListener("click", () => {
+        document.querySelector("#website_container").style.display = "initial";
         document.querySelector(".cart_container").style.display = "none";
-        console.log("Please login");
+    });
+
+    // some user is logged in
+    if (localStorage.getItem("loggedInEMail") !== null && localStorage.getItem("loggedInEMail") !== undefined && localStorage.getItem("loggedInEMail") !== "") {
+        let loggedInUser = JSON.parse(localStorage.getItem("user_details")).find(e => e.emailAddress === localStorage.getItem("loggedInEMail").replace(/['"]+/g, ''));
+        loginBtn.innerText = loggedInUser.fname;
     }
 
-    // logged in
-    if (currUser.cart.length) {
+    goToCartBtn.addEventListener("click", checkLoggedIn);
+    function checkLoggedIn() {
 
-        document.querySelector("#website_container").style.display = "none";
-        document.querySelector(".signUpContainer").style.display = "none";
-        document.querySelector(".admin_container").style.display = "none";
-        document.querySelector(".login_container").style.display = "none";
-        document.querySelector(".cart_container").style.display = "inherit";
-        document.querySelector("#noItems").style.display = "none";
+        let loggedInEMail = localStorage.getItem("loggedInEMail");
+        let users = JSON.parse(localStorage.getItem("user_details"));
+        let currUser = users.find(e => e.emailAddress === loggedInEMail.replace(/['"]+/g, ''));
 
-        let returnBtn = document.querySelector("#backToHome");
-        returnBtn.addEventListener("click", () => {
-            document.querySelector("#website_container").style.display = "initial";
+        // not logged in
+        if (loggedInEMail === "" || loggedInEMail === undefined || loggedInEMail === null) {
+            document.querySelector("#website_container").style.display = "none";
+            document.querySelector(".signUpContainer").style.display = "none";
+            document.querySelector(".admin_container").style.display = "none";
+            document.querySelector(".login_container").style.display = "inherit";
             document.querySelector(".cart_container").style.display = "none";
-        });
-
-        for (let i = 0; i < currUser.cart.length; i++) {
-
-            let cartProduct = document.createElement("div");
-            cartProduct.classList.add("cartProduct");
-
-            let image = document.createElement("img");
-            image.classList.add("productImageInCart");
-            image.setAttribute("src", currUser.cart[i].img);
-            cartProduct.appendChild(image);
-
-            let name = document.createElement("h2");
-            name.classList.add("nameOfProductInCart");
-            name.innerText = currUser.cart[i].name;
-            cartProduct.appendChild(name);
-
-            let price = document.createElement("p");
-            price.classList.add("priceOfProductInCart");
-            price.innerText = currUser.cart[i].price;
-            cartProduct.appendChild(price);
-
-            // let description = document.createElement("p");
-            // description.classList.add("descriptionOfProduct");
-            // description.appendChild(currUser.cart[i].uctDescription);
-            // cartProduct.appendChild(description);
-
-            let quantity = document.createElement("div");
-            var plusBtn = document.createElement("button");
-            plusBtn.classList.add("inc_dec_btn");
-            plusBtn.innerText = "+";
-            quantity.appendChild(plusBtn);
-            var minusBtn = document.createElement("button");
-            minusBtn.classList.add("inc_dec_btn");
-            minusBtn.innerText = "−";
-            quantity.appendChild(minusBtn);
-
-            let count = 1;
-            var countDisplay = document.createElement("span");
-            countDisplay.classList.add("countOfItem");
-            countDisplay.innerText = count;
-            plusBtn.addEventListener("click", (event) => {
-                count++;
-                event.target.parentNode.childNodes[2].innerText = count;
-            });
-            minusBtn.addEventListener("click", (event) => {
-                count--;
-                if (count === 0) {
-                    let userArr = JSON.parse(localStorage.getItem("user_details"));
-
-                    let cart = userArr.find(e => e.emailAddress === loggedInEMail.replace(/['"]+/g, '')).cart;
-                    let name = cart.find(e => e.name === event.target.parentNode.parentNode.childNodes[1].innerText);
-                    cart.splice(cart.indexOf(name), 1);
-                    localStorage.setItem("user_details", JSON.stringify(userArr));
-                    console.log(userArr);
-
-                    document.querySelector(".allProductsInCart").removeChild(event.target.parentNode.parentNode);
-                }
-                if (count > 1) {
-                    event.target.parentNode.childNodes[2].innerText = count;
-                }
-
-            });
-            quantity.appendChild(countDisplay);
-            cartProduct.appendChild(quantity);
-            console.log(cartProduct);
-
-            document.querySelector(".allProductsInCart").appendChild(cartProduct);
+            alert("Please login to go to cart");
         }
 
-    }
-}
+        // cart is not empty
+        if (currUser.cart.length) {
 
-if (localStorage.getItem("loggedInEMail") !== null && localStorage.getItem("loggedInEMail") !== undefined && localStorage.getItem("loggedInEMail") !== "") {
-    let loggedInUser = JSON.parse(localStorage.getItem("user_details")).find(e => e.emailAddress === localStorage.getItem("loggedInEMail").replace(/['"]+/g, ''));
-    loginBtn.innerText = loggedInUser.fname;
-}
-loginBtn.addEventListener("click", () => {
-    if (localStorage.getItem("loggedInEMail") === "" || localStorage.getItem("loggedInEMail") === null || localStorage.getItem("loggedInEMail") === undefined) {
-        document.querySelector(".login_container").style.display = "inherit";
-        document.querySelector("#website_container").style.display = "none";
-        document.querySelector(".signUpContainer").style.display = "none";
-        document.querySelector(".admin_container").style.display = "none";
-        document.querySelector(".cart_container").style.display = "none";
+            document.querySelector("#checkout").addEventListener("click",(event)=>{
+                currUser.cart.splice(0,currUser.cart.length);
+                localStorage.setItem("user_details", JSON.stringify(users));
+                alert("Thanks for buying");
+                document.querySelector("#website_container").style.display = "inherit";
+                document.querySelector(".signUpContainer").style.display = "none";
+                document.querySelector(".admin_container").style.display = "none";
+                document.querySelector(".login_container").style.display = "none";
+                document.querySelector(".cart_container").style.display = "none";
+                document.querySelector("#noItems").style.display = "none";
+                location.reload();
+            });
+
+            document.querySelector("#website_container").style.display = "none";
+            document.querySelector(".signUpContainer").style.display = "none";
+            document.querySelector(".admin_container").style.display = "none";
+            document.querySelector(".login_container").style.display = "none";
+            document.querySelector(".cart_container").style.display = "inherit";
+            document.querySelector("#noItems").style.display = "none";            
+
+            for (let i = 0; i < currUser.cart.length; i++) {
+                const element = currUser.cart[i];
+
+                let cartProduct = document.createElement("div");
+                cartProduct.classList.add("cartProduct");
+
+                let image = document.createElement("img");
+                image.classList.add("productImageInCart");
+                image.setAttribute("src", element.img);
+                cartProduct.appendChild(image);
+
+                let name = document.createElement("h2");
+                name.classList.add("nameOfProductInCart");
+                name.innerText = element.name;
+                cartProduct.appendChild(name);
+
+                let price = document.createElement("p");
+                price.classList.add("priceOfProductInCart");
+                price.innerText = element.price;
+                cartProduct.appendChild(price);
+
+                // let description = document.createElement("p");
+                // description.classList.add("descriptionOfProduct");
+                // description.appendChild(element.uctDescription);
+                // cartProduct.appendChild(description);
+
+                let quantity = document.createElement("div");
+                var plusBtn = document.createElement("button");
+                plusBtn.classList.add("inc_dec_btn");
+                plusBtn.innerText = "+";
+                var minusBtn = document.createElement("button");
+                minusBtn.classList.add("inc_dec_btn");
+                minusBtn.innerText = "−";
+                
+                quantity.appendChild(plusBtn);
+                quantity.appendChild(minusBtn);
+
+                let count = 1;
+                var countDisplay = document.createElement("span");
+                countDisplay.classList.add("countOfItem");
+                countDisplay.innerText = count;
+                plusBtn.addEventListener("click", (event) => {
+                    count++;
+                    event.target.parentNode.childNodes[2].innerText = count;
+                });
+                minusBtn.addEventListener("click", (event) => {
+                    count--;
+                    event.target.parentNode.childNodes[2].innerText = count;
+                    if (count === 0) {
+                        let userArr = JSON.parse(localStorage.getItem("user_details"));
+                        let cart = userArr.find(e => e.emailAddress === loggedInEMail.replace(/['"]+/g, '')).cart;
+                        let name = cart.find(e => e.name === event.target.parentNode.parentNode.childNodes[1].innerText);
+                        cart.splice(cart.indexOf(name), 1);
+                        localStorage.setItem("user_details", JSON.stringify(userArr));
+                        console.log(userArr);
+
+                        document.querySelector(".allProductsInCart").removeChild(event.target.parentNode.parentNode);
+                    }
+                });
+                
+                quantity.appendChild(countDisplay);
+                cartProduct.appendChild(quantity);
+                console.log(cartProduct);
+
+                document.querySelector(".allProductsInCart").appendChild(cartProduct);
+            }
+        }
+        else{
+            document.querySelector("#website_container").style.display = "none";
+            document.querySelector(".signUpContainer").style.display = "none";
+            document.querySelector(".admin_container").style.display = "none";
+            document.querySelector(".login_container").style.display = "none";
+            document.querySelector(".cart_container").style.display = "inherit";
+        }
     }
-});
+}
